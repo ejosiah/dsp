0424a22f9c7a0951209c0f78053b596ca7b45a40
Josiah Ebhomenye <joebhomenye@gmail.com>
2023-07-21 17:27:47 +0100

    updated audio engine eval

--- eval/engine_eval/main.cpp
+++ eval/engine_eval/main.cpp
@@ -1,89 +1,82 @@
 #include <audio/engine.h>
 #include <audio/choc_Oscillators.h>
+#include <audio/choc_AudioFileFormat_WAV.h>
 #include <random>
 #include <algorithm>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 #include <dsp/dsp.h>
+#include <audio/source.h>
+#include <memory>
+#include <chrono>
+
 #define SAMPLE_RATE (48000)
 #define T (1.0/SAMPLE_RATE)
 #define H (2.0 * dsp::PI)
 #define kH (1000 * H)
 #define FRAME_COUNT (512)
 
-int main(int, char**){
+enum class Choice : int { Noise = 1, SineWave, AudioPlayback};
+
+int main(int argc, char** argv){
+    using namespace std::chrono_literals;
+
+    if(argc == 1){
+        std::cout << "usage: [1|2|3] 1 for noise, 2 for sine wave, 3 for audio playback";
+        exit(0);
+    }
+
+    Choice selection = static_cast<Choice>(std::stoi(argv[1]));
+
     audio::Format format{0, 2, audio::SampleType::Float32, SAMPLE_RATE, FRAME_COUNT, SAMPLE_RATE};
     audio::Engine engine{format};
     engine.start();
-//    auto patchIn0 = engine.connectNewInput(2048);
-    auto patchIn1 = engine.connectNewInput(SAMPLE_RATE/10);
 
-    auto rng = [dist=std::uniform_real_distribution<float>{-1, 1}
-                , engine=std::default_random_engine{std::random_device{}()}]() mutable {
-        return dist(engine);
-    };
+    auto output = engine.connectNewInput(2048);
 
+    switch(selection){
+        case Choice::Noise: {
+            auto dist = std::uniform_real_distribution<float>(-1, 1);
+            auto eng = std::default_random_engine{ std::random_device{}() };
+            auto rng = [dist, eng ]() mutable {
+                return dist(eng);
+            };
 
-
-//    std::thread t0{[&]{
-//        const auto N = 1024;
-//        std::vector<float> samples(N);
-//        while(engine.isActive()) {
-//            std::generate(begin(samples), end(samples), [&] { return rng() * 0.01; });
-//            patchIn0.pushAudio(samples.data(), N);
-//            std::this_thread::sleep_for(std::chrono::milliseconds(2));
-//        }
-//    }};
-
-    choc::oscillator::Sine<float> oscillator;
-    oscillator.setFrequency(2500, SAMPLE_RATE);
-    choc::oscillator::Square<float> pause;
-    pause.setFrequency(5, SAMPLE_RATE);
-
-    std::thread t1{[&]{
-        const auto info = engine.info();
-//        const int N = SAMPLE_RATE;
-        const int N = 1024;
-        std::vector<float> samples(N);
-
-        while(info.isActive()){
-            for(int i = 0; i < N; i++){
-                auto sample = std::cosf(2.f * dsp::PI * (i * T) * 2500) * 0.01;
-                float amp = (i/(SAMPLE_RATE/10))%2;
-//                std::cout << "i: " << i << " amp: " << amp << "\n";
-                samples[i] = oscillator.getSample() * 0.01 * (.5 * pause.getSample() + .5);
-            }
-            patchIn1.pushAudio(samples.data(), N);
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
+            auto source = audio::continuous(output, [rng]() mutable { return rng(); });
+            source.play();
+            engine.sleep(10s);
+            source.stop();
+            break;
         }
-    }};
+        case Choice::SineWave: {
+            choc::oscillator::Sine<float> osc{};
+            osc.setFrequency(440, SAMPLE_RATE);
+
+            auto source = audio::continuous(output, [osc]() mutable { return osc.getSample(); });
+            source.play();
+            engine.sleep(10s);
+            source.stop();
+            break;
+        }
+        case Choice::AudioPlayback: {
+            std::string audioPath = "../../../../resources/voice.wav";
+            std::shared_ptr<std::istream> audioStream = std::make_shared<std::ifstream>(audioPath, std::ios::binary);
+
+            choc::audio::WAVAudioFileFormat<false> audioFileFormat{};
+            auto reader = audioFileFormat.createReader(audioStream);
+            auto buffer = reader->readEntireStream<float>();
+            auto iter = buffer.getIterator(0);
+            auto clip = audio::clip(output, std::span{ iter.sample, buffer.getSize().numFrames });
+            clip.loop(3);
+            clip.play();
+            engine.sleep(10s);
+            break;
+        }
+    }
 
 
-
-   engine.sleep(std::chrono::milliseconds(5000));
-   engine.shutdown();
-
-//    t0.join();
-    t1.join();
-
-
-
-
-//    audio::CircularAudioBuffer<float> buffer(4);
-//    std::vector<float> bucket(10);
-//    std::iota(begin(bucket), end(bucket), 0);
-//    auto pushed = buffer.push(bucket.data(), 6);
-//
-//    std::cout << "pushed " << pushed << "\n";
-//    std::cout << buffer.remainder() << " available to write\n";
-//    std::cout << buffer.num() << " available to read\n";
-//
-//    auto read = buffer.pop(bucket.data(), 4);
-//    for(int i = 0; i < read; i++){
-//        std::cout << bucket[i] << "\n";
-//    }
+    engine.shutdown();
 
     return 0;
 }
