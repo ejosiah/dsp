97a584c162ab4d975d534453a0a50717e5634dbb
Josiah Ebhomenye <joebhomenye@gmail.com>
2023-07-21 17:44:12 +0100

    added wind to audio engine evall

--- eval/engine_eval/main.cpp
+++ eval/engine_eval/main.cpp
@@ -10,6 +10,7 @@
 #include <audio/source.h>
 #include <memory>
 #include <chrono>
+#include "wind_generator.h"
 
 #define SAMPLE_RATE (48000)
 #define T (1.0/SAMPLE_RATE)
@@ -17,13 +18,13 @@
 #define kH (1000 * H)
 #define FRAME_COUNT (512)
 
-enum class Choice : int { Noise = 1, SineWave, AudioPlayback};
+enum class Choice : int { Noise = 1, SineWave, Wind, AudioPlayback};
 
 int main(int argc, char** argv){
     using namespace std::chrono_literals;
 
     if(argc == 1){
-        std::cout << "usage: [1|2|3] 1 for noise, 2 for sine wave, 3 for audio playback";
+        std::cout << "usage: [1|2|3] 1 for noise, 2 for sine wave, 3 for Wind, 4 for audio playback";
         exit(0);
     }
 
@@ -56,6 +57,23 @@ int main(int argc, char** argv){
             auto source = audio::continuous(output, [osc]() mutable { return osc.getSample(); });
             source.play();
             engine.sleep(10s);
+            source.stop();
+            break;
+        }
+        case Choice::Wind: {
+            WindGenerator windGenerator{ static_cast<uint32_t>(SAMPLE_RATE) };
+            Whistling wh0{ 400, 600, 1.2, 100, static_cast<uint32_t>(SAMPLE_RATE), 0.12f };
+            Whistling wh1{ 1000, 1000, 2.0, 1000, static_cast<uint32_t>(SAMPLE_RATE) };
+            TreeLeaves treeLeaves{static_cast<uint32_t>(SAMPLE_RATE)};
+            Howls howls0{ static_cast<uint32_t>(SAMPLE_RATE), 100, std::make_tuple(0.35f, 0.6f), 0.5, 400, 40, 0.35f, 30.f, 200};
+            Howls howls1{ static_cast<uint32_t>(SAMPLE_RATE), 300, std::make_tuple(0.25f, 0.5f), 0.1, 200, 40, 0.25f, 20.f, 100};
+
+            auto source = audio::continuous(output, [&]() mutable {
+               return windGenerator.getSample() +  wh0.getSample() + wh1.getSample()
+                        + howls0.getSample() + howls1.getSample() + treeLeaves.getSample();
+            });
+            source.play();
+            engine.sleep(60s);
             source.stop();
             break;
         }
